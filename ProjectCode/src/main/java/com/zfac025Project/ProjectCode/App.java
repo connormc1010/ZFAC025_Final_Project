f0a7d89b27bb12bcafe0e00ca33818324b523669
Connor <zfac025@live.rhul.ac.uk>
2021-11-30 12:54:18 +0000

    Constructed maven project

---
+++ ProjectCode/src/main/java/com/zfac025Project/ProjectCode/App.java
@@ -0,0 +1,13 @@
+package com.zfac025Project.ProjectCode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
