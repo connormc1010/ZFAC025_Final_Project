27c4dafe28e610a4bcd357dd852ba7264e114e65
Connor <zfac025@live.rhul.ac.uk>
2021-11-30 16:54:33 +0000

    Removed unused method in SSHBrute force class

--- ProjectCode/src/main/java/com/zfac025Project/ProjectCode/AttackerScripts/SSHBruteForce.java
+++ ProjectCode/src/main/java/com/zfac025Project/ProjectCode/AttackerScripts/SSHBruteForce.java
@@ -1,13 +1,6 @@
 package com.zfac025Project.ProjectCode.AttackerScripts;
 
-import java.util.function.BooleanSupplier;
-
 public class SSHBruteForce {
-
-	public BooleanSupplier attack() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	public Object scan() {
 		// TODO Auto-generated method stub
