7a7612aad44f200426e1e49b08ab8e8e0fc6630c
Connor <zfac025@live.rhul.ac.uk>
2021-11-30 16:41:41 +0000

    Victim able to search to outlook.co.uk and apple.com

--- ProjectCode/src/main/java/com/zfac025Project/ProjectCode/GoogleChromeOperations.java
+++ ProjectCode/src/main/java/com/zfac025Project/ProjectCode/GoogleChromeOperations.java
@@ -1,10 +1,18 @@
 package com.zfac025Project.ProjectCode;
 
+import org.openqa.selenium.By;
+
 public class GoogleChromeOperations extends GoogleChrome{
 
 
-	public boolean getWebsite(String url) {
+	public boolean getWebsite(String url) throws InterruptedException {
 		driver.get(url);
+		driver.wait(10);
+		return true;
+	}
+	
+	public boolean clickElement(String xpath) {
+		driver.findElements(By.xpath(xpath)).get(0).click();
 		return true;
 	}
 
--- ProjectCode/src/test/java/com/zfac025Project/ProjectCode/VictimScripts/VictimScriptTest.java
+++ ProjectCode/src/test/java/com/zfac025Project/ProjectCode/VictimScripts/VictimScriptTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.WebDriver;
 import com.zfac025Project.ProjectCode.GoogleChrome;
 import com.zfac025Project.ProjectCode.GoogleChromeOperations;
@@ -13,6 +15,7 @@ import com.zfac025Project.ProjectCode.GoogleChromeOperations;
 /**
  * Unit test for simulating a victims actions on a network
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class VictimScriptTest 
 {
 	
@@ -34,8 +37,21 @@ public class VictimScriptTest
 	
 	@Test
 	@Order(1)
-	public void bbcNewsWebsite() {
+	public void bbcNewsWebsite() throws InterruptedException {
 		assertTrue(operations.getWebsite("https://www.bbc.co.uk"));
 		}
+	
+	@Test
+	@Order(2)
+	public void outlook() throws InterruptedException {
+		assertTrue(operations.getWebsite("https://outlook.office.com"));
+	}
+	
+	@Test
+	@Order(3)
+	public void apple() throws InterruptedException {
+		assertTrue(operations.getWebsite("https://www.apple.com"));
+		assertTrue(operations.clickElement("//*[@id=\"ac-globalnav\"]/div/ul[2]/li[2]/a"));
+	}
     
 }
